942cf37a483d7e9b1e0cccabe18d31239474d112
Your Name <you@example.com>
2021-11-15 09:51:07 -0700

    about changes

--- src/components/pages/about.js
+++ src/components/pages/about.js
@@ -16,12 +16,9 @@ export default function() {
       />
       <div className="right-column">
         <div className="content">
-          <h1>Lorme Ipsum</h1>
-          <p>dolor sit amet consectetur, adipisicing elit. Perferendis magnam nisi, animi maiores praesentium 
-            quidem distinctio repellendus recusandae eum alias tempore dignissimos aspernatur sunt, nulla saepe! Quae, 
-            obcaecati totam temporibus aspernatur, laboriosam ut,  ratione sint impedit sunt voluptate voluptatibus corrupti 
-            eum pariatur nemo! Saepe enim commodi atque fugiat exercitationem sed quae, laborum a, quibusdam rerum laudantium delectus non unde. Debitis?
-          </p>
+          <h1>I'm Sean Peterson</h1>
+          <p> I am a Highly motivated and dedicated Full Stack Developer with professional experience in background and hands-on experience in development and implementation of customized software and web-based application. Experienced in collaborating with cross- functional teams in all stages of software development, scripting and creating scalable website applications. Proficient at implementing core backend development tasks including 3rd party API integrations while working with multiple frameworks, software, Operating systems and programming languages.
+        </p>
 
         </div>
       </div>
